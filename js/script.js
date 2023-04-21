97ec424e0ebbca2815790b7e584fc99db20c949a
Shirokov Roman <r0man_sh@mail.ru>
2023-04-21 23:20:19 +0300

    21.04

--- js/script.js
+++ js/script.js
@@ -1,206 +1,3 @@
-let val = []
-let x = 1
-let y = 1
-let inputs = document.querySelectorAll('.input__file');
-let countFiles1 = 0;
-
-document.addEventListener('click', e => {
-    
-    Array.prototype.forEach.call(inputs, function (input) {
-        let label = input.nextElementSibling,
-            labelVal = label.querySelector('.input__file-button-text').innerText;
-    
-        input.addEventListener('change', function (e) {
-            let countFiles = '';
-            if (this.files && this.files.length >= 1)
-                countFiles = this.files.length;
-    
-            if (countFiles)
-                label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
-            else
-                label.querySelector('.input__file-button-text').innerText = labelVal;
-        });
-    });
-    
-    const element = document.getElementById('accChange')
-    const elementE = e.composedPath().includes(element)
-
-    const element1 = document.getElementById('accSave')
-    const element1E = e.composedPath().includes(element1)
-
-    const element2 = document.getElementById('accCancel')
-    const element2E = e.composedPath().includes(element2)
-
-    const element3 = document.getElementById('addRecipe')
-    const element3E = e.composedPath().includes(element3)
-
-    const ChangeInp = document.querySelectorAll('.wrapper .input-group input');
-
-    if (element3E) {
-
-
-        Array.prototype.forEach.call(inputs, function (input) {
-
-            input.addEventListener('change', function (e) {
-
-                if (this.files && this.files.length == 0) countFiles1 = 0
-
-                if (this.files && this.files.length != 0) countFiles1 = 1
-
-
-                if (countFiles1 == 0)
-                    document.querySelector('.wrapper div[class="alerts"]').style.display = "block"
-
-                if (countFiles1 != 0)
-                    document.querySelector('.wrapper div[class="alerts"]').style.display = "none"
-            });
-
-            if (countFiles1 == 0)
-                document.querySelector('.wrapper div[class="alerts"]').style.display = "block"
-        });
-
-    }
-
-    for (let i = 0; i < ChangeInp.length; i++) { // выведет 0, затем 1, затем 2
-        val.push(ChangeInp[i].value)
-    }
-
-
-    if (elementE) {
-        for (let i = 0; i < ChangeInp.length; i++) { // выведет 0, затем 1, затем 2
-            ChangeInp[i].removeAttribute("disabled");
-            ChangeInp[i].style.backgroundColor = "rgb(252,231,140)"
-        }
-
-        document.getElementById('accChange').style.display = "none"
-        document.getElementById('accSave').style.display = "block"
-        document.getElementById('accCancel').style.display = "block"
-    }
-
-    else if (element1E) {
-        for (let i = 0; i < ChangeInp.length; i++) { // выведет 0, затем 1, затем 2
-            ChangeInp[i].setAttribute("disabled", "true");
-            ChangeInp[i].style.backgroundColor = "black"
-        }
-
-        val = []
-
-        for (let j = 0; j < ChangeInp.length; j++) { // выведет 0, затем 1, затем 2
-            val.push(ChangeInp[j].value)
-        }
-
-        document.getElementById('accChange').style.display = "inline"
-        document.getElementById('accSave').style.display = "none"
-        document.getElementById('accCancel').style.display = "none"
-    }
-
-    else if (element2E) {
-
-
-        for (let j = 0; j < ChangeInp.length; j++) { // выведет 0, затем 1, затем 2
-            ChangeInp[j].value = val[j]
-        }
-
-        for (let i = 0; i < ChangeInp.length; i++) { // выведет 0, затем 1, затем 2
-            ChangeInp[i].setAttribute("disabled", "true");
-            ChangeInp[i].style.backgroundColor = "black"
-        }
-
-        document.getElementById('accChange').style.display = "inline"
-        document.getElementById('accSave').style.display = "none"
-        document.getElementById('accCancel').style.display = "none"
-    }
-})
-
-let steps = []
-let products = []
-
-function addInput() {
-    if (x < 10) {
-        let str = '<div class="input-group" style="width: 100%;"><p style="width: 20%; padding-top: 5px;">Продукт:</p><input id ="product' + (x + 1) + '" style="width: 40%; border-radius: .375rem; height: 5vh;"  type="text" class="form-control"><p style="width: 20%; padding-top: 5px;">Количество:</p><input id ="product' + (x + 1) + 'quantity" style="width: 20%; border-radius: .375rem; height: 5vh;"  type="text" class="form-control"></div><div id="input' + (x + 1) + '"></div>'
-        document.getElementById('input' + x).innerHTML = str;
-        products.push(document.getElementById('input' + x))
-        x++;
-    }
-    else {
-        alert('Нельзя добавить больше ингридиентов!');
-    }
-}
-
-function removeInput() {
-    if (products.length > 0) {
-        let poped = products.length
-        products.pop()
-        x = x - 1
-        document.getElementById('input' + poped).innerHTML = "";
-    }
-}
-
-function addInputSteps() {
-    if (y < 15) {
-        var str = '<div class="row" style="border: 2px solid rgb(252,231,140); border-radius: 50px; padding: 10px 0px 30px 0px; margin-bottom: 50px;">' +
-            '<h3>Шаг ' + (y + 1) + '</h3>' +
-            '<hr>' +
-            '<div class="file2 col-5" style="padding: 5px 0px 0px 0px;">' +
-            '<p style="width: 20%; margin: 0px 0px 15px 70px;">Фото:</p>' +
-            '<div class="input__wrapper" style="margin-left: 5%; margin-bottom: 0;">' +
-            '<input name="file" id="input__file'+(y + 1) +'" type="file" class="input input__file">' +
-            '<label for="input__file'+(y+1)+'" class="input__file-button">' +
-            '<p class="input__file-icon-wrapper"><i class="fa-solid fa-paperclip"></i></p>' +
-            '<p class="input__file-button-text">Выберите файл</p>' +
-            '</label>' +
-            '</div>' +
-            '</div>' +
-            '<div class="col-7">' +
-            '<p style="width: 20%; padding-top: 5px;">Описание:</p>' +
-            '<div class="textarea">' +
-            '<div class="scrolling">' +
-            '<div class="text" id = "stepDesc' + (y + 1) + '" contenteditable="true"></div>' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '</div><div id="inputSteps'+(y + 1) +'" class="container"></div>'
-
-
-
-        document.getElementById('inputSteps' + y).innerHTML = str;
-        steps.push(document.getElementById('inputSteps' + y))
-        y++;
-        inputs = document.querySelectorAll('.input__file');
-    }
-    else {
-        alert('Нельзя добавить больше шагов!');
-    }
-}
-
-function removeInputSteps() {
-    if (steps.length > 0) {
-        let poped = steps.length
-        steps.pop()
-        y = y - 1
-        document.getElementById('inputSteps' + poped).innerHTML = "";
-    }
-}
-
-
-
-Array.prototype.forEach.call(inputs, function (input) {
-    let label = input.nextElementSibling,
-        labelVal = label.querySelector('.input__file-button-text').innerText;
-
-    input.addEventListener('change', function (e) {
-        let countFiles = '';
-        if (this.files && this.files.length >= 1)
-            countFiles = this.files.length;
-
-        if (countFiles)
-            label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
-        else
-            label.querySelector('.input__file-button-text').innerText = labelVal;
-    });
-});
-
-
 
 function registration() {
     let title = document.getElementById('modal-content')
@@ -261,7 +58,7 @@ function registration() {
     '                                       <button type="submit" class="btn btn-primary btn-log-in btn-sign-up">Зарегистрироваться</button>\n' +
     '                                   </div>' +
     '                                </form>'
-};
+}
 
 function login() {
     let title = document.getElementById('modal-content')
@@ -287,7 +84,7 @@ function login() {
         '                                        <button type="submit" class="btn btn-primary btn-log-in btn-sign-in">Войти</button>\n' +
         '                                    </div>\n' +
         '                                </form>'
-};
+}
 
 function scrolling(clicked) {
     window.scrollTo(0, 2000);
@@ -330,3 +127,58 @@ function proportion_pm(clicked) {
     }
 }
 
+const acc_change = document.getElementById('accChange')
+const acc_save = document.getElementById('accSave')
+const acc_cancel = document.getElementById('accCancel')
+const ChangeInp = document.querySelectorAll('.recipe-block .input-group input');
+const pass = document.getElementById('password')
+const pass_repeat = document.getElementById('password_repeat')
+let val = []
+let gen_val = []
+function Change_acc_data() {
+    for (let i = 0; i < ChangeInp.length; i++) {
+        ChangeInp[i].removeAttribute("disabled");
+        ChangeInp[i].style.backgroundColor = "rgb(252,231,140)"
+    }
+    for (let i = 0; i < ChangeInp.length; i++) {
+        val.push(ChangeInp[i].value)
+    }
+
+    document.getElementById('password_zone').style.display = "flex"
+    console.log(ChangeInp)
+    document.getElementById('accChange').style.display = "none"
+    document.getElementById('accSave').style.display = "block"
+    document.getElementById('accCancel').style.display = "block"
+}
+function Save_acc_data() {
+    if (pass.value === pass_repeat.value) {
+        for (let i = 0; i < ChangeInp.length; i++) {
+            ChangeInp[i].setAttribute("disabled", "true");
+            ChangeInp[i].style.backgroundColor = "rgb(233,236,239)"
+        }
+        val = []
+        document.getElementById('password_zone').style.display = "none"
+        document.getElementById('accChange').style.display = "flex"
+        document.getElementById('accSave').style.display = "none"
+        document.getElementById('accCancel').style.display = "none"
+    }
+    else alert('Пароли должны совпадать!')
+}
+
+function Cancel_acc_data() {
+    for (let j = 0; j < ChangeInp.length; j++) {
+        ChangeInp[j].value = val[j]
+    }
+    for (let i = 0; i < ChangeInp.length; i++) {
+        ChangeInp[i].setAttribute("disabled", "true");
+        ChangeInp[i].style.backgroundColor = "rgb(233,236,239)"
+    }
+
+    document.getElementById('password_zone').style.display = "none"
+    document.getElementById('accChange').style.display = "inline"
+    document.getElementById('accSave').style.display = "none"
+    document.getElementById('accCancel').style.display = "none"
+}
+acc_change.onclick = Change_acc_data
+acc_save.onclick = Save_acc_data
+acc_cancel.onclick = Cancel_acc_data
